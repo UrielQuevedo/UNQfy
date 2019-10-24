a6eec79b32d16144bd47e43a55f3e5f9413c8e1f
Dani Villegas <dani.villegascarp@gmail.com>
2019-10-24 03:30:47 -0300

    add allArtists and putArtistById

--- routes/artists.js
+++ routes/artists.js
@@ -24,7 +24,23 @@ router.get('/:id', (req, res) => {
     }
 }); 
 
+router.get('/', (req, res) => {
+        res.status(200).json(unqfy.artists);
+}); 
 
-
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const newName = req.body.name;
+    const newCountry = req.body.country;
+    try {
+        const artist = unqfy.getArtistById(id);
+        artist.name = newName;
+        artist.country = newCountry;
+        res.status(200).json(artist);
+     }
+    catch {
+        res.status(404).json({status:404, errorCode:"RESOURCE_NOT_FOUND"});
+    }
+});
 
 module.exports = router;

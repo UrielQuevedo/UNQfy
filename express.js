316f04b8655d207c1d3d91d9945641d988fe5ab9
Dani Villegas <dani.villegascarp@gmail.com>
2019-10-24 00:41:55 -0300

    add express and router

---
+++ express.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 8080); 
+app.set('json spaces', 2);
+
+// middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); 
+
+// routes 
+app.use('/',require('./routes/index'));
+
+// starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port',app.get('port'));
+});

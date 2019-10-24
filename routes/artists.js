c76b3abe7cb93499702f18805d1cc67f313f9ba2
Dani Villegas <dani.villegascarp@gmail.com>
2019-10-24 03:13:09 -0300

    add getArtistById

--- routes/artists.js
+++ routes/artists.js
@@ -9,8 +9,22 @@ router.post('/', (req, res) => {
          res.status(201).json(unqfy.addArtist(newArtist));
     }
     catch {
-        res.status(409).json({status: 409, errorCode:"RESOURCE_ALREADY_EXISTS"});
+        res.status(409).json({status:409, errorCode:"RESOURCE_ALREADY_EXISTS"});
     }
 });
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    try {
+        const artist = unqfy.getArtistById(id);
+        res.status(200).json(artist);
+    }
+    catch {
+        res.status(404).json({status:404, errorCode:"RESOURCE_NOT_FOUND"});
+    }
+}); 
+
+
+
 
 module.exports = router;

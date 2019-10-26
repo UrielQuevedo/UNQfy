e55f901d366692173e2203d86eb55f20becbc0f5
Dani Villegas <dani.villegascarp@gmail.com>
2019-10-26 18:37:32 -0300

    user can listen track

--- routes/users.js
+++ routes/users.js
@@ -58,5 +58,31 @@ router.delete('/:id', (req, res) => {
   }
 });
 
+router.get('/:id/tracksHeard', (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = unqfy.getUser(id);
+    res.status(200).json(user.tracksHeard);
+  }
+  catch(error) {
+    res.status(404).json({status:404, errorCode:'RESOURCE_NOT_FOUND'});
+  }
+}); 
 
+// se asume que el track ya esta creado en UNQfy
+
+router.post('/:id/tracksHeard', (req, res) => {
+  const { id } = req.params;
+  const newTrack = req.body;
+  console.log(newTrack);
+  try { 
+    const user = unqfy.getUser(id);
+    user.listenTrack(newTrack);
+    res.status(201).json(newTrack);
+  }
+  catch(error) {
+    res.status(404).json({status:404, errorCode:'RESOURCE_NOT_FOUND', message: error.message});
+  }
+});
+  
 module.exports = router;

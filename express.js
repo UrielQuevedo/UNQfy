0b4daccca398ca0a563ed9026b3fad3664669e2a
Dani Villegas <dani.villegascarp@gmail.com>
2019-10-29 15:57:40 -0300

    add error invalid json

--- express.js
+++ express.js
@@ -14,8 +14,13 @@ app.use('/api/artists',require('./routes/artists'));
 app.use('/api/tracks',require('./routes/tracks'));
 app.use('/api/users',require('./routes/users'));
 app.use('/invalidRoute',require('./routes/invalidRoute'));
+app.use((err, req, res, next) => {
+  if (err.type === 'entity.parse.failed') {
+    res.status(400).json({status:400, errorCode:'BAD_REQUEST'});
+  }
+});
 
 // starting the server
 app.listen(app.get('port'), () => {
-    console.log('Server on port',app.get('port'));
+  console.log('Server on port',app.get('port'));
 });
